79265929b500f27410309f4a30f5800bef83374e
hellonagi <38994662+hellonagi@users.noreply.github.com>
2024-02-20 15:26:11 +0900

    add deck and card class

--- war.rb
+++ war.rb
@@ -3,6 +3,7 @@ class Game
 
   def initialize
     @players = []
+    @deck = Deck.new
   end
 
   def start_game(player_count: 2)
@@ -12,6 +13,9 @@ class Game
 
     # 開始メッセージ
     puts '戦争を開始します。'
+
+    # カードを配る
+    @deck.deal_cards(@players)
   end
 
   def make_players
@@ -22,9 +26,52 @@ class Game
 end
 
 class Player
+  attr_reader :hand
+
   def initialize(num)
     @name = "プレイヤー#{num}"
     @hand = []
+  end
+end
+
+class Deck
+  attr_reader :cards
+
+  SUITS = [:クラブ, :ダイヤ, :ハート, :スペード]
+  RANKS = (2..10).to_a + [:A, :J, :Q, :K]
+
+  def initialize
+    @cards = []
+    SUITS.each do |suit|
+      RANKS.each do |rank|
+        @cards << Card.new(suit, rank)
+      end
+    end
+    @cards = @cards.shuffle
+  end
+
+  def deal_cards(players)
+    num_card = @cards.size / players.size
+
+    players.each do |player|
+      num_card.times do
+        player.hand << @cards.pop
+      end
+    end
+
+    puts 'カードが配られました。'
+  end
+end
+
+class Card
+  attr_reader :value
+
+  CARD_HASH = { A: 99, J: 11, Q: 12, K: 13 }
+
+  def initialize(suit, rank)
+    @suit = suit
+    @rank = rank
+    @value = CARD_HASH[rank] || rank
   end
 end
 
--- war_test.rb
+++ war_test.rb
@@ -7,7 +7,41 @@ class GameTest < MiniTest::Test
   end
 
   def test_start_game
-    assert_output("戦争を開始します。\n") { @game.start_game }
+    assert_output("戦争を開始します。\nカードが配られました。\n") { @game.start_game }
+
+    # プレイヤー数は2人か
     assert_equal 2, @game.players.size
+
+    # プレイヤーの手札数はそれぞれ26枚か
+    @game.players.each do |player|
+      assert_equal 26, player.hand.size
+    end
+  end
+end
+
+class DeckTest < MiniTest::Test
+  def setup
+    @deck = Deck.new
+  end
+
+  # デッキのカード枚数は52か
+  def test_deck_has_52_cards
+    assert_equal 52, @deck.cards.size
+  end
+end
+
+class CardTest < MiniTest::Test
+  # J,Q,K,Aがそれぞれ11, 12, 13, 99という値を持っているか
+  def test_card_rank_has_expected_value
+    card_2 = Card.new(:ダイヤ, 2)
+    assert_equal 2, card_2.value
+    card_j = Card.new(:ダイヤ, :J)
+    assert_equal 11, card_j.value
+    card_q = Card.new(:ダイヤ, :Q)
+    assert_equal 12, card_q.value
+    card_k = Card.new(:ダイヤ, :K)
+    assert_equal 13, card_k.value
+    card_a = Card.new(:ダイヤ, :A)
+    assert_equal 99, card_a.value
   end
 end
